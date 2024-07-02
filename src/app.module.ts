b5956e4ecb719f31640fb2fa7ff0e6d465217f15
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-07-02 20:17:32 +0900

    Init project

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LinkModule } from './link/link.module';
+import { UserModule } from './user/user.module';
+import { LinkExtrasModule } from './link-extras/link-extras.module';
+
+@Module({
+   imports: [
+      TypeOrmModule.forRoot({
+         type: 'mysql',
+         host: 'localhost',
+         port: 3306,
+         username: 'root',
+         password: 'root',
+         database: 'whitebox',
+         synchronize: true,
+      }),
+      LinkModule,
+      UserModule,
+      LinkExtrasModule,
+   ],
+   controllers: [AppController],
+   providers: [AppService],
+})
+export class AppModule {}
